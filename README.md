91cba94237843ab587d9167e607e34d57b90c4ec
wi <diabloevagto@gmail.com>
2022-01-08 14:49:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# solana-amm-swap-exercise
+
+## TODO
+
+- [ ] 連接錢包
+- [ ] 取得錢包 token 及餘額
+- [ ] 連接 raydium swap
+- [ ] 儲存常用交易對
---
+++ src/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>solana-amm-swap-exercise</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
